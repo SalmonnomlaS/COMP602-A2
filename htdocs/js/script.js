207f3ddd9bf026d8f6b3fe1fa0133a45e4bdd72b
SalmonnomlaS <127051504+SalmonnomlaS@users.noreply.github.com>
2024-04-08 17:07:40 +1200

    Added preloader

---
+++ htdocs/js/script.js
@@ -0,0 +1,9 @@
+const preloader = document.querySelector(".preloader");
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        preloader.classList.add("hidden");
+        navbar.classList.add("show-nav");
+    }, 2000);
+});
