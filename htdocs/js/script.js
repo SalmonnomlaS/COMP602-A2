b04abf1f3c914f4ff28a94365b9ab0e7da4ef3a2
SalmonnomlaS <127051504+SalmonnomlaS@users.noreply.github.com>
2024-04-29 15:00:12 +1200

    Added services row inverted sides

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -9,19 +9,21 @@ window.addEventListener("load", () => {
 });
 
 
-let button = document.getElementById("topBtn");
+let button = document.getElementById("topBtn");//Get button ID
 
 window.onscroll = function() {scrollFunction()};
 
+//Display the button when not at top of page
 function scrollFunction() {
     if (document.documentElement.scrollTop > 20 || document.body.scrollTop > 20) {
         button.style.display = "block";
-    } else {
+    }
+    else {
         button.style.display = "none";
     }
 }
-
+//Support for all browsers
 function backToTop() {
-    document.body.scrollTop = 0;//Safari
-    document.documentElement.scrollTop = 0;//Chrome, Firefox, IE and Opera
-} 
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
