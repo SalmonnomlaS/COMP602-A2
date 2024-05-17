40ae962920c8aa6245db91f18b298827b9b5a43d
SalmonnomlaS <127051504+SalmonnomlaS@users.noreply.github.com>
2024-05-17 13:19:00 +1200

    yep

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -6,7 +6,7 @@ window.addEventListener("load", () => {
     setTimeout(() => {
         preloader.classList.add("hidden");
         navbar.classList.add("show-nav");
-    }, 150);
+    }, 1500);
 });
 
 //--------Back to top button--------
